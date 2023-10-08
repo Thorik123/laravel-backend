51e17ef5c4033e06957b99950730097af97725af
Thorik123 <tronikthorik07@gmail.com>
2023-10-09 06:26:14 +0700

    create auth logout api

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
