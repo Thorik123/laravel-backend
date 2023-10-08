af02ecd3a31f536ecbe9d9296854ec7b6f1d6065
Thorik123 <tronikthorik07@gmail.com>
2023-10-09 02:22:19 +0700

    add page user-list with pagination

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,6 +31,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('profile-edit', function () {
         return view('pages.profile', ['type_menu' => '']);
     })->name('profile.edit');
+
+    //==================================================================
+    Route::resource('user', UserController::class);
 });
 
 // Route::get('/login', function () {
