5e04f7064874b4775fc92a912e9b5702ba16c123
Thorik123 <tronikthorik07@gmail.com>
2023-10-09 01:16:54 +0700

    add feature edit-profile

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//======================================================================
 Route::get('/', function () {
     return view('auth.login', ['type_menu' => '']);
 });
 
+//======================================================================
 Route::middleware(['auth', 'verified'])->group(function () {
+
+    //==================================================================
     Route::get('home', function () {
-        return view('pages.blank-page', ['type_menu' => '']);
+        return view('pages.dashboard', ['type_menu' => '']);
     })->name('home')->middleware('can:dashboard');
+
+    //==================================================================
+    Route::get('profile-edit', function () {
+        return view('pages.profile', ['type_menu' => '']);
+    })->name('profile.edit');
 });
 
 // Route::get('/login', function () {
