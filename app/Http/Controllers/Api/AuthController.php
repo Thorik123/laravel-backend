2d9bbba82176bd2749589a77cce15ae733835cec
Thorik123 <tronikthorik07@gmail.com>
2023-10-09 06:46:35 +0700

    create auth register api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -11,6 +11,7 @@ use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
+    //====================================================================
     public function login(Request $request)
     {
         $request->validate([
@@ -39,6 +40,30 @@ class AuthController extends Controller
         ]);
     }
 
+    //====================================================================
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required | string | email',
+            'password' => 'required',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'user',
+        ]);
+
+        $token = $user->createToken('api-token')->plainTextToken;
+        return response()->json([
+            'jwt-token' => $token,
+            'user' => new UserResource($user),
+        ]);
+    }
+
+    //====================================================================
     public function logout(Request $request)
     {
         $request->user()->tokens()->delete();
