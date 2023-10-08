32d37e34e0442841eca991a7ea6165fa4c611db6
Thorik123 <tronikthorik07@gmail.com>
2023-10-09 04:05:39 +0700

    create api to show data category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -34,9 +34,10 @@ class CategoryController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Category $category)
     {
-        //
+        $category->load('products');
+        return $category;
     }
 
     /**
