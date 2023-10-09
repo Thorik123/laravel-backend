2b55749a3130f2eda3ddbcf8662501ff76bc6ff3
Thorik123 <tronikthorik07@gmail.com>
2023-10-09 23:32:33 +0700

    create order api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UploadController;
 use Illuminate\Http\Request;
@@ -32,6 +33,10 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 Route::post('image/upload', [UploadController::class, 'uploadImage'])
     ->middleware('auth:sanctum');
 Route::post('image/upload-multiple', [UploadController::class, 'uploadMultipleImage'])
+    ->middleware('auth:sanctum');
+
+//======================================================================================
+Route::post('orders', [OrderController::class, 'order'])
     ->middleware('auth:sanctum');
 
 //======================================================================================
